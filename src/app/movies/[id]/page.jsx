55c9b88026e8804d4098ae930cfe0aca83113acb
Putra2203 <erdin22xii@gmail.com>
2025-02-03 23:17:19 +0700

    movie details

---
+++ src/app/movies/[id]/page.jsx
@@ -0,0 +1,116 @@
+import Card from "@/components/Card";
+import Certification from "@/components/Certification";
+import CustomButton from "@/components/CustomButton";
+import { getRecDetails } from "@/services/movie-rec";
+import { getMovieDetails } from "@/services/movie-service";
+
+export default async function movieDetails({ params }) {
+  const { id } = params;
+  const movie = await getMovieDetails(id);
+
+  const recommendations = await getRecDetails(id);
+
+  const certification = movie.certification || "";
+  const cast = movie.cast ? movie.cast : [];
+
+  const indonesiaData = movie.releaseDates?.results?.find(
+    (result) => result.iso_3166_1 === "ID"
+  );
+  const releaseDate = indonesiaData?.release_dates[0]?.release_date || "";
+  const releaseYear = releaseDate ? releaseDate.substring(0, 4) : "N/A";
+
+  function formatRuntime(minutes) {
+    const hours = Math.floor(minutes / 60);
+    const mins = minutes % 60;
+    return `${hours}h ${mins}m`;
+  }
+  const runtimeFormatted = movie.runtime ? formatRuntime(movie.runtime) : "N/A";
+
+  return (
+    <div className="container max-w-screen-xl mx-auto h-fit">
+      <div className="movie-details-wrapper flex flex-col">
+        <div className="video-section flex w-full">
+          <img
+            src={`${process.env.NEXT_APP_BASEIMG}${movie.backdropPath}`}
+            alt="image-details"
+            className="object-cover object-top rounded-lg"
+          />
+        </div>
+        <div className="overview-cast-wrapper flex mt-10 gap-20">
+          <div className="left-content flex flex-col gap-4">
+            <h1 className="text-4xl tracking-wide">{movie.title}</h1>
+            <div className="flex gap-5 ">
+              <CustomButton type="add" size="large" />
+              <CustomButton type="watched" size="large" />
+            </div>
+            <div className=" flex items-center gap-2">
+              <Certification result={certification} />
+              <p>
+                {movie.genres.join(", ")} ● {releaseYear}
+              </p>
+            </div>
+            <p className="">Duration {runtimeFormatted}</p>
+            <p className="text-pretty">{movie.overview}</p>
+            <div className="grid grid-cols-3 border-t border-slate-600 pt-4">
+              <p className="">Director</p>
+              <p className="text-pretty col-span-2">
+                {movie.directors.join(", ")}
+              </p>
+            </div>
+            <div className="grid grid-cols-3 border-t border-slate-600 pt-4 ">
+              <p className="">Writers</p>
+              <p className="text-pretty col-span-2">
+                {movie.writers.join(", ")}
+              </p>
+            </div>
+            <div className="grid grid-cols-3 border-t border-slate-600 pt-4">
+              <p className="">Studios</p>
+              <p className="text-pretty col-span-2">
+                {movie.companyNames.join(", ")}
+              </p>
+            </div>
+          </div>
+          <div className="right-content flex flex-col w-1/2">
+            <div className="gap-2 flex">
+              <p className="border-l-8 border-secondary"></p>
+              <h2 className="text-4xl font-bold">CAST</h2>
+            </div>
+            <div className="cast-list items-center grid grid-rows-2 grid-flow-col gap-5 overflow-x-auto mt-4">
+              {cast.map((actor, index) => (
+                <div
+                  key={index}
+                  className="text-sm flex flex-col text-center gap-1 w-fit"
+                >
+                  <div className="w-40 h-40 flex">
+                    {actor.profile_path ? (
+                      <img
+                        src={`${process.env.NEXT_APP_BASEIMG}${actor.profile_path}`}
+                        alt={actor.name}
+                        className="rounded-2xl object-cover"
+                      />
+                    ) : (
+                      <div className="w-40 h-40 rounded-2xl bg-gray-300 flex items-center justify-center">
+                        <span className="text-sm text-gray-600">No Image</span>
+                      </div>
+                    )}
+                  </div>
+                  <p className="font-semibold text-xs">{actor.name}</p>
+                  <p className="text-xs">{actor.character}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col mt-10 gap-4">
+          <div className="gap-2 flex">
+            <p className="border-l-8 border-secondary"></p>
+            <h2 className="text-4xl font-bold">RECOMMENDATIONS</h2>
+          </div>
+          <div>
+            <Card results={recommendations} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/CustomButton.jsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React from "react";
+import { PlusCircle, Eye } from "@phosphor-icons/react";
+
+const CustomButton = ({ type, size = "medium", className = "" }) => {
+  const buttonConfig = {
+    add: {
+      text: "Add Watchlist",
+      icon: <PlusCircle size={20} className="inline-block mr-2" />,
+      className: "bg-white text-black hover:bg-secondary hover:text-white",
+    },
+    watched: {
+      text: "Watched It",
+      icon: <Eye size={20} className="inline-block mr-2" />,
+      className:
+        "bg-transparent border border-secondary border-4 text-secondary hover:bg-secondary hover:text-white",
+    },
+  };
+
+  const { text, icon, className: variantClassName } = buttonConfig[type] || {};
+
+  const sizeClasses = {
+    small: "py-1 w-10 text-sm",
+    medium: "py-2 w-20 text-base",
+    large: "py-1 w-52 text-2xl",
+  };
+
+  return (
+    <button
+      className={` rounded-lg transition-colors font-bebas_neue duration-200 flex items-center justify-center    ${variantClassName} ${sizeClasses[size]} ${className}`}
+    >
+      {icon}
+      {text}
+    </button>
+  );
+};
+
+export default CustomButton;
